2502f5490778f50069b82d61b051bd99757890cd
Russ Berg <russ.berg@lightsource.ca>
2025-04-21 10:13:37 -0600

    final pass

--- bcm/backend.py
+++ bcm/backend.py
@@ -1,6 +1,8 @@
 
-# this file simply delcares the name of the backend to be used, it is purely just this variable so
+# this generated file simply delcares the name of the backend to be used, it is purely just this variable so
+
 # that it can simply be imported and checked by other files
-# set the backend that should be used, CLS=epics, SLS=zmq
-# BACKEND='epics'
+
+# set the backend that should be used
+
 BACKEND='zmq'

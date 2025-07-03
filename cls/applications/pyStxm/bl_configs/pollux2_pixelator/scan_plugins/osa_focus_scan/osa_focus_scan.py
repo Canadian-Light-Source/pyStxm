44a3299ef0ccb38e965b56ab5702e9b3f4d3ab90
bergr <russ.berg@lightsource.ca>
2025-07-03 11:59:05 -0600

    added new config for Pollux2

---
+++ cls/applications/pyStxm/bl_configs/pollux2_pixelator/scan_plugins/osa_focus_scan/osa_focus_scan.py
@@ -0,0 +1,29 @@
+"""
+Created on 04/11/2022
+
+@author: bergr
+"""
+
+from cls.applications.pyStxm.main_obj_init import MAIN_OBJ, DEFAULTS
+from cls.data_io.stxm_data_io import STXMDataIo
+from cls.utils.log import get_module_logger
+
+from cls.applications.pyStxm.bl_configs.base_scan_plugins.osa_focus_scan.osa_focus_scan import (
+    BaseOsaFocusScanParam,
+)
+from cls.applications.pyStxm.bl_configs.pixelator_common.plugin_utils import init_scan_req_member_vars
+
+_logger = get_module_logger(__name__)
+
+
+class OsaFocusScanParam(BaseOsaFocusScanParam):
+
+    data = {}
+
+    def __init__(self, parent=None):
+        super().__init__(main_obj=MAIN_OBJ, data_io=STXMDataIo, dflts=DEFAULTS)
+        self.scan_class = self.instanciate_scan_class(
+            __file__, "OsaFocusScan", "OsaFocusScanClass"
+        )
+        init_scan_req_member_vars(self)
+

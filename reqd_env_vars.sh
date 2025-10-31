73bd5d842255fe07db4a6c80ba88c1e549834631
bergr <russ.berg@lightsource.ca>
2025-10-31 15:19:44 -0600

    cleaned up,

--- reqd_env_vars.sh
+++ reqd_env_vars.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
-
+# required for EPICS only, if using Pixelator only you can leave these as is
 export OPHYD_CONTROL_LAYER="caproto"
 
-# this is the host name of th ecomputer that is running the DCS (Pixelator Controller)
-export DCS_HOST="localhost"
+# this is the host name of the computer that is running the DCS (Pixelator Controller)
+export DCS_HOST="IP ADDR HERE"
 export DCS_HOST_PROC_NAME="Pixelator"
 export DCS_SUB_PORT="56561"
 export DCS_REQ_PORT="56562"
 
-# This is the IP address of the computer that has the binary cnxvalidate on it, note this is only relevant for BlueSky configurations
-# Pixelator configs save their own files
-export CNXVALIDATE_HOST_IPADDR="IP ADDR HERE"
-export PATH_TO_CNXVALIDATE="DIR PATH HERE"
 #this is the IP addr of the computer that is running the NXserver or Pixelator depending on your setup
 export DATA_SERVER_HOST="IP ADDR HERE"
+# this is the port that the data will be published on for pyStxm to display
+export DATA_SUB_PORT="56563"
 
+# this is the base path to the data
+# NOTE: for Pixelator this path must match the path configured in the Pixelator software settings.json file
+# for the entries `NeXusBaseDirectory` and `NeXusDiscardSubDirectory`
+export DATA_DIR="/tmp"
 
-# this is the path to the directory that contains the nexus definitions cloned repo
+# This is the IP address of the computer that has the binary cnxvalidate on it,
+# note this is only relevant for BlueSky configurations
+export CNXVALIDATE_HOST_IPADDR="IP ADDR HERE"
+export PATH_TO_CNXVALIDATE="DIR PATH HERE"
+# this is the path to the directory that contains the nexus definitions cloned repo, Only needed if planning
+# to validate nexus files using cnxvalidate via pyStxm
 export PATH_TO_NX_DEFINITIONS="PATH HERE"
-
-# if pyStxm is run on windows then the path to the main data directory will needs to be specified here as a windows path
-export PYSTXM_DATA_DIR="/tmp"
-# this is the path used by the areaDetector plugin to save the data for tomography scans, NOTE:
-export LINUX_DATA_DIR="/tmp"
-
-
-
-
